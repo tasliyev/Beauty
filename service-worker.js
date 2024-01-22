8a329063344445352675aa9c9f4d6d8d1b16f29d
tasliyev <157374026+tasliyev@users.noreply.github.com>
2024-01-22 17:41:09 +0500

    Fgh
    Fgh

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+// service-worker.js
+
+const CACHE_NAME = 'spa-beauty-salon-v1';
+const urlsToCache = [
+  '/',
+  'index.html', // Include other HTML files if needed
+  'style.css',
+  'service-worker.js', // Make sure to include the service worker itself
+  // Add other static assets (images, etc.) here
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
